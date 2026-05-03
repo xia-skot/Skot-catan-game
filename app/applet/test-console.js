b321ff5ce886c5648ce04c9b0fa53da699db1129
xia-skot <651412826@qq.com>
2026-05-04 02:31:24 +0800

    Add files via upload

---
+++ app/applet/test-console.js
@@ -0,0 +1,10 @@
+const { chromium } = require('playwright');
+(async () => {
+  const browser = await chromium.launch();
+  const page = await browser.newPage();
+  page.on('console', msg => console.log('PAGE LOG:', msg.text()));
+  page.on('pageerror', err => console.log('PAGE ERROR:', err.message));
+  await page.goto('http://localhost:3000');
+  await page.waitForTimeout(3000);
+  await browser.close();
+})();
--- src/images.ts
+++ src/images.ts
@@ -1,5 +1,22 @@
+import { ResourceType } from './types';
+
 export const FOREST_IMG = '/api/proxy-image?url=' + encodeURIComponent('http://tdyuzzmmy.hn-bkt.clouddn.com/img/u7AhKge - Imgur.jpg');
 export const FIELDS_IMG = '/api/proxy-image?url=' + encodeURIComponent('http://tdyuzzmmy.hn-bkt.clouddn.com/img/yG8cXii - Imgur.jpg');
 export const PASTURE_IMG = '/api/proxy-image?url=' + encodeURIComponent('http://tdyuzzmmy.hn-bkt.clouddn.com/img/O0rOQKN - Imgur.jpg');
 export const Desert_IMG =  '/api/proxy-image?url=' + encodeURIComponent('http://tdyuzzmmy.hn-bkt.clouddn.com/img/bHGjRHQ - Imgur.jpg');
 export const Mountains_IMG =  '/api/proxy-image?url=' + encodeURIComponent('http://tdyuzzmmy.hn-bkt.clouddn.com/img/ieLNUXP - Imgur.jpg');
+
+// Resource Icons
+export const LUMBER_ICON = '/api/proxy-image?url=' + encodeURIComponent('http://tdyuzzmmy.hn-bkt.clouddn.com/img/树.png');
+export const BRICK_ICON = '/api/proxy-image?url=' + encodeURIComponent('http://tdyuzzmmy.hn-bkt.clouddn.com/img/砖块.png');
+export const WOOL_ICON = '/api/proxy-image?url=' + encodeURIComponent('http://tdyuzzmmy.hn-bkt.clouddn.com/img/羊.png');
+export const GRAIN_ICON = '/api/proxy-image?url=' + encodeURIComponent('http://tdyuzzmmy.hn-bkt.clouddn.com/img/小麦.png');
+export const ORE_ICON = '/api/proxy-image?url=' + encodeURIComponent('http://tdyuzzmmy.hn-bkt.clouddn.com/img/铁矿石2.png');
+
+export const RESOURCE_ICONS: Record<ResourceType, string> = {
+  [ResourceType.Lumber]: LUMBER_ICON,
+  [ResourceType.Brick]: BRICK_ICON,
+  [ResourceType.Wool]: WOOL_ICON,
+  [ResourceType.Grain]: GRAIN_ICON,
+  [ResourceType.Ore]: ORE_ICON,
+};
